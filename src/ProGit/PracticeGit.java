0a10811b30863f56aa0bd7b6bc6343b808869bb0
unknown <faisal.amanzai@tekschool.us>
2022-11-08 21:57:47 -0800

    new commit

---
+++ src/ProGit/PracticeGit.java
@@ -0,0 +1,16 @@
+package ProGit;
+
+public class PracticeGit {
+
+	public static void main(String[] args) {
+		
+		
+		int x = 45;
+		int y = 25;
+		System.out.println(x + y);
+		
+	
+		}
+	}
+
+
---
+++ src/ProGit/package-info.java
@@ -0,0 +1,1 @@
+package ProGit;
